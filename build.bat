97198bbd604436b44f7ec1741ec384e126f21845
Valeria Iegorova <iegorval@gmail.com>
2025-10-23 14:44:06 +0200

    Updated MRE

--- build.bat
+++ build.bat
@@ -4,6 +4,8 @@ setlocal enabledelayedexpansion
 echo Starting Nuitka build...
 set "STARTTIME=%TIME%"
 
+pip freeze
+
 python -m nuitka ^
     --standalone ^
     --enable-plugin=tk-inter ^
@@ -14,21 +16,37 @@ python -m nuitka ^
 
 set "ENDTIME=%TIME%"
 
-rem --- Convert times to hundredths of a second ---
-for /f "tokens=1-4 delims=:.," %%a in ("%STARTTIME%") do (
-    set /a "start=(((%%a*60)+%%b)*60+%%c)*100+%%d"
+rem --- Parse time (remove leading spaces and handle both . and , separators) ---
+for /f "tokens=1-4 delims=:., " %%a in ("%STARTTIME%") do (
+    set "h=%%a"
+    set "m=%%b"
+    set "s=%%c"
+    set "cs=%%d"
 )
-for /f "tokens=1-4 delims=:.," %%a in ("%ENDTIME%") do (
-    set /a "end=(((%%a*60)+%%b)*60+%%c)*100+%%d"
+for /f "tokens=1-4 delims=:., " %%a in ("%ENDTIME%") do (
+    set "h2=%%a"
+    set "m2=%%b"
+    set "s2=%%c"
+    set "cs2=%%d"
 )
 
-rem Handle midnight wrap
-if %end% lss %start% set /a end+=24*60*60*100
+rem --- Remove possible leading spaces ---
+for %%v in (h m s cs h2 m2 s2 cs2) do (
+    set "val=!%%v!"
+    for /f "tokens=* delims= " %%x in ("!val!") do set "%%v=%%x"
+)
 
-set /a elapsed=%end%-%start%
-set /a hh=%elapsed% / 360000, rem=%elapsed% %% 360000
-set /a mm=%rem% / 6000, rem=%rem% %% 6000
-set /a ss=%rem% / 100, cs=%rem% %% 100
+rem --- Convert to hundredths of a second ---
+set /a "start=((h*60+m)*60+s)*100+cs"
+set /a "end=((h2*60+m2)*60+s2)*100+cs2"
+
+rem --- Handle midnight wrap ---
+if !end! lss !start! set /a end+=24*360000
+
+set /a elapsed=!end!-!start!
+set /a hh=!elapsed!/360000, rem=!elapsed!%%360000
+set /a mm=!rem!/6000, rem=!rem!%%6000
+set /a ss=!rem!/100, cs=!rem!%%100
 
 echo.
 echo ======================================
